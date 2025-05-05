ba7adb75f6b4219ab988b5c94babecaaf9f38f18
Hancooler <76528360+Hancooler@users.noreply.github.com>
2025-05-05 19:10:09 +0700

    course

---
+++ combining comparator/index.js
@@ -0,0 +1,24 @@
+prompt("what is your name?");
+prompt ("what is their name?");
+
+var loveScore = Math.random() = 100;
+loveScore = Math.floor(loveScore) +1; //1-100
+
+if (loveScore > 70) {
+    alert("your love score is " + loveScore + "%" + "you love each Other like kanye love loves kanye.");
+    
+}
+if (loveScore > 30 && lovescore <=70){
+    alert("your love score is " + loveScorecore + "%");
+}
+
+if (loveScore <=30) {
+    alert("your love score is " + loveScore + "%" + "you go togehter live oil and water");
+}
+
+
+
+else{
+    alert("your love score is " + loveScore + "%");
+    
+}
---
+++ comparator/index.js
@@ -0,0 +1,13 @@
+prompt("what is your name?");
+prompt ("what is their name?");
+
+var loveScore = Math.random() = 100;
+loveScore = Math.floor(loveScore) +1; //1-100
+
+if (loveScore > 70) {
+    alert("your love score is " + loveScore + "%" + "you love each Other like kanye love loves kanye.");
+    
+}else{
+    alert("your love score is " + lovescore + "%");
+    
+}
