0e608193d5567da6dfa194e037f253d554de38c9
Hancooler <76528360+Hancooler@users.noreply.github.com>
2025-05-21 15:21:45 +0700

    array

---
+++ array/index.js
@@ -0,0 +1,9 @@
+var guestList = ["hans","migel","newman","jams","ondu","jui"];
+
+var guestName = prompt("what is your name");
+
+if (guestList.includes(guestName)) {
+    alert("welcome");
+}else{
+    alert("sorry kamu harus ketemu salman");
+}
