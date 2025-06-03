c54b5a73a1b1d6a239c109326d3c13d00df8f918
Hancooler <76528360+Hancooler@users.noreply.github.com>
2025-06-03 09:48:44 +0700

    js

---
+++ condition/index.js
@@ -0,0 +1,15 @@
+prompt("what is your name?");
+prompt("what is their name?");
+
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if (loveScore > 70) {
+    alert("your love score is " + loveScore + "%, you are a perfect match!");
+}else {
+    alert("your love score is " + loveScore + "%");
+}
+
+alert("Your love score is " + loveScore + "%");
+
